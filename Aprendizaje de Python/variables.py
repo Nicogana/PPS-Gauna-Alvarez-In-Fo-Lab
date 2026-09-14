d9e62293ab7749fc2db1294cf10b515b1060b80b
Nicogana <gaunanicolasg@gmail.com>
2026-09-14 11:51:53 -0300

    Subo variables

---
+++ Aprendizaje de Python/variables.py
@@ -0,0 +1,18 @@
+# Para el capítulo de Variables, utilizamos este archivo:
+# Iniciamos con un Hello World! como en el otro.
+
+print("Hello World!")
+
+# Luego, iniciamos el procesamiento con variables
+
+message= "Hello Python World!"
+print(message)
+
+# A Python no le interesa si está escrito correctamente o no, solo que sean iguales
+
+mesaje = "Hello my friend"
+print(mesaje)
+
+# Al cambiar la variable, se modifica su contenido. 
+message = "HELLO WORLD FASTA"
+print(message)
