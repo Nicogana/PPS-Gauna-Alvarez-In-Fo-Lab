0d8d43bd644027c731225bdbfd0a3ba7686f07b2
cyor-byte <alvarezjosematias1@gmail.com>
2026-09-14 11:45:23 -0300

    Ejemplos introductorios de Strings en Python

---
+++ Aprendizaje de Python/name.py
@@ -0,0 +1,14 @@
+# Aprendimos sobre el uso de cadenas (Strings) que son conjuntos de caracteres.
+
+"Esto es una cadena"
+'Esto también es una cadena'
+
+"Las cadenas pueden tener 'comillas simples'"
+'Y también "comillas dobles"'
+
+# Existen múltiples métodos para formatear cadenas
+
+nombre = "san martin"
+print(nombre.title()) # imprime San Martin
+print(nombre.upper()) # imprime SAN MARTIN
+print(nombre.lower()) # imprime san martin  
