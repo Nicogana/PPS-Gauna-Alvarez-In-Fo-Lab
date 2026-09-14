70d0087482b81a67b90cf37fb7530547f530b56a
Nicogana <gaunanicolasg@gmail.com>
2026-09-14 12:19:02 -0300

    Apostrofos

---
+++ Aprendizaje de Python/apostrofo.py
@@ -0,0 +1,8 @@
+# En cuanto a los apóstrofos, primero se observa que si intenrcambiamos los ' entre "" se muestra el mensaje normal
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# Pero si se utiliza entre ' ', ocurre un error pues luego del apóstrofo intermedio, no reconoce que siga la cadena.
+
+message = 'One of Python's strengths is its diverse community.'
+print(message)
