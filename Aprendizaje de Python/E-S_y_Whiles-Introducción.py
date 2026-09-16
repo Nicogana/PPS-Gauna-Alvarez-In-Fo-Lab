65dabeea297623b6fa4cedfdb830504e79373a1d
cyor-byte <alvarezjosematias1@gmail.com>
2026-09-16 11:21:36 -0300

    Completado el Capítulo 8

--- Aprendizaje de Python/E-S_y_Whiles-Introducción.py
+++ Aprendizaje de Python/E-S_y_Whiles-Introducción.py
@@ -11,6 +11,10 @@ edad = input("Decime tu edad por favor: ") # Por ejemplo, 35
 
 edad = int(edad) # Para que ande hay que castearlo como entero
 
+# esto también es válido
+
+edad = int(input("decime tu edad, por favor: "))
+
 print(edad + 1) # Ahora si, anda e imprime 36
 
 # Otro bucle particularmente interesante es el While. A diferencia del for, que se ejecuta n veces, el while se ejecuta 
@@ -55,3 +59,77 @@ while activo: # La bandera activo es la que determina si el programa se sigue ej
 # while True:
       # print("Esto nunca para")
 
+# Podemos usar un break para salir del bucle
+
+while True: 
+      echo = input("Digite salir para salir: ")
+
+      if echo == 'salir':
+            break
+      else: 
+            print(f"digitaste: {echo}")
+
+# Tambien existe el continue, que complementa al break
+# Le indica a python que no debe debe regresar al 
+# principio del bucle sin ejecutar el resto de su cuerpo
+
+numero = 0
+
+while numero < 10: 
+      numero += 1
+      if numero % 2 == 0:
+            continue
+      print(f"Este numero ({numero}) es impar")
+
+
+usuarios_no_confirmados = ['Bruno', 'Ana', 'Luis']
+usuarios_confirmados = []
+
+# En general, los bucles while se utilizan cuando no sabemos 
+# cuantas iteraciones se deben realizar
+
+while usuarios_no_confirmados: 
+      usuario = usuarios_no_confirmados.pop()
+
+      print(f"Verificando usuario: {usuario.title()}")
+      usuarios_confirmados.append(usuario)
+
+      print("\n Lista de usuarios confirmados:  ")
+
+# Y los bucles for se utilizan cuando si sabemos
+# cuantas iteraciones se deben realizar 
+
+for usuario_confirmado in usuarios_confirmados:
+        print(usuario_confirmado.title())
+
+
+# Tambien se usan para remover duplicados en una lista
+
+mascotas = ['perro', 'gato', 'caballo', 'surubí', 'gato', 'conejo']
+print(mascotas)
+
+while 'gato' in mascotas:
+      mascotas.remove('gato')
+
+print(mascotas)
+
+# Y para llenar diccionarios
+
+encuesta_activa = True
+
+respuestas_encuesta = {}
+
+while encuesta_activa:
+      nombre = input("¿Como te llamas?: ")
+      montaña = input("¿que montaña te gustaría escalar?: ")
+
+      respuestas_encuesta[nombre] = montaña
+
+      repetir = input("¿Desea compartir la encuesta? (s/n)")
+      if repetir == 'no':
+            encuesta_activa = False
+
+print("\n --- Resultados de la Encuesta ---")
+
+for nombre, montaña in respuestas_encuesta.items():
+      print(f"a {nombre} le gustaria escalar {montaña}")
