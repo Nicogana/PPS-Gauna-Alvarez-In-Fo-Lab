9ce6cf8d658017fabf80d7f7cb7d1929e335d946
cyor-byte <alvarezjosematias1@gmail.com>
2026-09-17 11:19:21 -0300

    Completada la introducción de Funciones

--- Aprendizaje_de_Python/Funciones-Introducción.py
+++ Aprendizaje_de_Python/Funciones-Introducción.py
@@ -123,14 +123,120 @@ while True:
     print("(Presione s para salir)")
 
     nombre = input("Nombre: ")
-    if nombre == 'q':
+    if nombre == 's':
             break
     apellido = input("Apellido: ")
-    if apellido == 'q':
+    if apellido == 's':
             break
 
     nombre_completo = formatear_nombre(nombre, apellido)
 
     print(f"Hola {nombre_completo}")
 
-    
+# Tambien podemos pasarle listas a las funciones
+
+def saludar_usuarios(usuarios):
+     for usuario in usuarios:
+          msj =  f"Hola {usuario.title()}"
+          print(msj)
+
+usuarios = ['Nico', 'Ivo', 'Santi', 'Jose']
+
+saludar_usuarios(usuarios)
+
+# Y estas pueden modificar los contenidos de la lista si lo deseamos
+
+def pagar_sueldos(sueldos_pendientes, sueldos_pagos):
+     while sueldos_pendientes:
+          sueldo = sueldos_pendientes.pop()
+          print(f"Pagando el sueldo a: {sueldo}")
+          sueldos_pagos.append(sueldo)
+
+def listar_sueldos_pagos(sueldos_pagos):
+     print("\n Los siguientes sueldos ya han sido pagos: ")
+     for sueldo in sueldos_pagos:
+          print(sueldo)
+
+# Podríamos mover la definición de las funciones a otro archivo 
+# y que en el cuerpo nos quede solo esto, que es muy facil de leer
+
+pendientes = ['Jose', 'Maria', 'Evelyn', 'Ramiro']
+pagos = []
+
+pagar_sueldos(pendientes, pagos)
+listar_sueldos_pagos(pagos)
+
+# Por supuesto, esto presenta un inconveniente. 
+# La lista original (pendientes)ahora está vacía. 
+
+print(pendientes) # Imprime []
+
+# En los casos en los que queremos preservar la lista original, 
+# podemos pasar una copia como argumento de la funcion
+
+pendientes = ['Jose', 'Maria', 'Evelyn', 'Ramiro']
+pagos = []
+
+pagar_sueldos(pendientes[:], pagos) # paso una copia de pendientes y la lista pagos
+
+print(pagos) # pagos se modifica 
+print(pendientes) # pendientes se preserva
+
+# Salvo que sea absolutamente necesario preservar la lista original, 
+# conviene pasar las listas originales como parametro
+# La copia preserva la lista original, pero es mucho mas lenta, 
+# porque debe copiar todo el contenido de la lista antes de arrancar la ejecución
+
+# Hay situaciones en las cuales no sabemos antes de tiempo 
+# cuantos argumentos tiene la función. 
+# Afortunadamente, podemos indicarle a la funcion 
+# que acepte tantos argumentos como le de el usuario
+# estos argumentos se denominan "Argumentos Arbitrários"
+
+def hacer_pizza(*ingredientes):  
+     if 'ananá' in ingredientes or 'piña' in ingredientes:
+        print("no.")
+     else:
+        print("Preparando una pizza con: ")
+        for ingrediente in ingredientes:
+            print(f"- {ingrediente}")
+          
+hacer_pizza('Tomate', 'Queso') 
+hacer_pizza('Tomate', 'Aceitunas', 'Jamón', 'Queso') 
+
+# Podemos mezclar args. arbit. y args. posicionales
+# Primero debemos poner los argumentos posicionales y a lo último los arbitrarios
+
+def hacer_pizza(tamaño, *ingredientes):  
+     if 'ananá' in ingredientes or 'piña' in ingredientes:
+        print("no.")
+     else:
+        print(f"Preparando una pizza {tamaño.lower()} con: ")
+        for ingrediente in ingredientes:
+            print(f"- {ingrediente.title()}")
+
+hacer_pizza("mediana", "Jamón", "Queso")
+
+# El orden importa: 
+# El valor mediana se utiliza para llenar el primer parámetro (tamaño)
+# Todos los valores adicionales se juntan dentro del argumento arbitrario *ingredientes
+# En general, a estos argumentos arbitrarios se los junta en el parámetro genérico *args
+
+# Ahora vamos con un concepto muy interesante. 
+# Podemos pasar tantos argumentos arbitrarios explicitos como querramos
+# Esto suena un poco raro, pero básicamente significa que podemos definir argumentos
+# Que no están especificados dentro de la funcion y pasarles valores. 
+# Esto se entiende mejor con un ejemplo:
+
+def crear_perfil(nombre, apellido, **datos):
+    datos['nombre'] = nombre
+    datos['apellido'] = apellido
+    return datos
+
+fisico = crear_perfil('Juan Martín', 'Maldacena', edad=58, educado_en='Balseiro')
+print(fisico) # Imprime {'edad': 58, 'educado_en': 'Balseiro', 
+              #          'nombre': 'Juan Martín', 'apellido': 'Maldacena'}
+
+# Como vemos, agregamos campos nuevos al diccionario, que no estaban originalmente,
+# utilizando los argumentos arbitrarios explicitos
+# estos se suelen agrupar bajo el nombre genérico de **kwargs
